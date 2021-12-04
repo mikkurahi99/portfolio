8f7361a21104bc6d6d54db2363c90d5c13b08a05
mikkurahi99 <84957997+mikkurahi99@users.noreply.github.com>
2021-12-04 13:33:53 +0530

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+var submit = document.getElementById('submit');
+
+submit.onclick =  function(){
+    alert('Message Sent Successfully!');
+};
+
+var resend = document.getElementById('resent');
+
+resend.onclick = function(){
+    alert('Message Resent!');
+}
